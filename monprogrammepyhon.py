9985056c6769e723a14b8552d51ef7897087cec9
celia234 <37139486+celia234@users.noreply.github.com>
2018-03-08 10:12:41 +0100

    Update and rename test1.py to monprogrammepyhon.py

---
+++ monprogrammepyhon.py
@@ -0,0 +1,13 @@
+liste1 = [[1, 4, 6, 0, 36, 309, "A"], ["A", "C", 9, "G"]]
+print(liste1[1][2])
+
+print ("helloword")
+
+def factorielle(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorielle(n - 1)
+print(factorielle(3))       
+        
+        
